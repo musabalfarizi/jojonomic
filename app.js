96f4542182cf5de89b684c595bb3cf94639b2b20
Mus Ab Alfarizi <musabalfarizi@gmail.com>
2021-01-18 10:43:43 +0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const router = require('./routes')
-const cors = require('corss')
+const cors = require('cors')
 
 mongoose.connect(process.env.MONGODB, {
     useNewUrlParser: true,

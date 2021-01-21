1a54564abb9ed1289a135764e03a0fc7085c7ade
Mus Ab Alfarizi <musabalfarizi@gmail.com>
2021-01-21 22:09:01 +0700

    "musab"

---
+++ my-app/blog-cms/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var auth = require('./routes/auth');
+var category = require('./routes/category');
+var post = require('./routes/post');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+mongoose.connect('mongodb://localhost/blog-cms', {
+    promiseLibrary: require('bluebird'),
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() =>  console.log('connection successful'))
+  .catch((err) => console.error(err));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/auth', auth);
+app.use('/api/category', category);
+app.use('/api/post', post);
+
+module.exports = app;
